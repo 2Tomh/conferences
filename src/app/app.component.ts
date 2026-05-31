2092b93131fe7fdc18b6b48f73d47962e2d0f3ae
Tom <2tom.hayoun@gmail.com>
2026-05-31 18:33:05 +0300

    updating he to en

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +9,21 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'mind-il';
   currentLang = 'en';
-constructor(private translate: TranslateService) {
-  this.translate.addLangs(['en', 'he']);
-  this.translate.setDefaultLang('en');
-  this.translate.use('en');
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'he']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
 
-  // הגדר כיוון בטעינה ראשונית ← חדש
-  document.documentElement.dir = 'ltr';
-  document.documentElement.lang = 'en';
+    // הגדר כיוון בטעינה ראשונית ← חדש
+    document.documentElement.dir = 'ltr';
+    document.documentElement.lang = 'en';
 
-  this.translate.onLangChange.subscribe(event => {
-    this.currentLang = event.lang;
-    document.documentElement.dir = event.lang === 'he' ? 'rtl' : 'ltr';
-    document.documentElement.lang = event.lang;
-  });
-}
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+      document.documentElement.dir = event.lang === 'he' ? 'rtl' : 'ltr';
+      document.documentElement.lang = event.lang;
+    });
+  }
 
   switchLang(lang: 'en' | 'he') {
     this.currentLang = lang;
