d584afeaf0eb2393c317bb1e56ab8dbb126e9bc0
Tom <2tom.hayoun@gmail.com>
2026-05-31 18:41:20 +0300

    updating he to en

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,38 @@
+// import { Component } from '@angular/core';
+// import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'mind-il';
+//   currentLang = 'en';
+//   constructor(private translate: TranslateService) {
+//     this.translate.addLangs(['en', 'he']);
+//     this.translate.setDefaultLang('en');
+//     this.translate.use('en');
+
+//     // הגדר כיוון בטעינה ראשונית ← חדש
+//     document.documentElement.dir = 'ltr';
+//     document.documentElement.lang = 'en';
+
+//     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+//       this.currentLang = event.lang;
+//       document.documentElement.dir = event.lang === 'he' ? 'rtl' : 'ltr';
+//       document.documentElement.lang = event.lang;
+//     });
+//   }
+
+//   switchLang(lang: 'en' | 'he') {
+//     this.currentLang = lang;
+//     this.translate.use(lang);
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { LanguageTransformService } from './services/language-transform.service';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +41,5 @@ import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'mind-il';
-  currentLang = 'en';
-  constructor(private translate: TranslateService) {
-    this.translate.addLangs(['en', 'he']);
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-
-    // הגדר כיוון בטעינה ראשונית ← חדש
-    document.documentElement.dir = 'ltr';
-    document.documentElement.lang = 'en';
-
-    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      this.currentLang = event.lang;
-      document.documentElement.dir = event.lang === 'he' ? 'rtl' : 'ltr';
-      document.documentElement.lang = event.lang;
-    });
-  }
-
-  switchLang(lang: 'en' | 'he') {
-    this.currentLang = lang;
-    this.translate.use(lang);
-  }
+  constructor(private langService: LanguageTransformService) {}
 }
--- src/app/services/language-transform.service.ts
+++ src/app/services/language-transform.service.ts
@@ -1,4 +1,56 @@
-import { Injectable, RendererFactory2, Renderer2 } from '@angular/core';
+// import { Injectable, RendererFactory2, Renderer2 } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+// import { TranslateService } from '@ngx-translate/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class LanguageTransformService {
+//   private renderer: Renderer2;
+//   private isEnglishSubject = new BehaviorSubject<boolean>(false);
+//   isEnglish$ = this.isEnglishSubject.asObservable();
+
+// constructor(
+//   rendererFactory: RendererFactory2,
+//   private translate: TranslateService
+// ) {
+//   this.renderer = rendererFactory.createRenderer(null, null);
+//   this.translate.addLangs(['he', 'en']);
+//   this.translate.setDefaultLang('he');
+//   this.translate.use('he');
+  
+//   this.renderer.addClass(document.body, 'rtl');
+// }
+
+//   toggleLanguage() {
+//     const nextState = !this.isEnglishSubject.value;
+//     this.isEnglishSubject.next(nextState);
+//     this.updateGlobalStyles(nextState);
+
+//     const lang = nextState ? 'en' : 'he';
+//     this.translate.use(lang);
+//   }
+
+//   private updateGlobalStyles(isEnglish: boolean) {
+//     const dir = isEnglish ? 'ltr' : 'rtl';
+//     const lang = isEnglish ? 'en' : 'he';
+
+//     this.renderer.setAttribute(document.documentElement, 'dir', dir);
+//     this.renderer.setAttribute(document.documentElement, 'lang', lang);
+//     this.renderer.setAttribute(document.body, 'dir', dir);
+//     document.body.style.direction = dir;
+
+//     if (isEnglish) {
+//       this.renderer.removeClass(document.body, 'rtl');
+//       this.renderer.addClass(document.body, 'ltr');
+//     } else {
+//       this.renderer.removeClass(document.body, 'ltr');
+//       this.renderer.addClass(document.body, 'rtl');
+//     }
+//   }
+// }
+
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -6,46 +58,23 @@ import { TranslateService } from '@ngx-translate/core';
   providedIn: 'root'
 })
 export class LanguageTransformService {
-  private renderer: Renderer2;
   private isEnglishSubject = new BehaviorSubject<boolean>(false);
   isEnglish$ = this.isEnglishSubject.asObservable();
 
-constructor(
-  rendererFactory: RendererFactory2,
-  private translate: TranslateService
-) {
-  this.renderer = rendererFactory.createRenderer(null, null);
-  this.translate.addLangs(['he', 'en']);
-  this.translate.setDefaultLang('he');
-  this.translate.use('he');
-  
-  this.renderer.addClass(document.body, 'rtl');
+constructor(private translate: TranslateService) {
+  this.translate.addLangs(['en', 'he']);
+  this.translate.setDefaultLang('he');  // ← he במקום en
+  this.translate.use('he');             // ← he במקום en
+  document.documentElement.dir = 'rtl'; // ← rtl במקום ltr
+  document.documentElement.lang = 'he'; // ← he במקום en
 }
 
   toggleLanguage() {
     const nextState = !this.isEnglishSubject.value;
     this.isEnglishSubject.next(nextState);
-    this.updateGlobalStyles(nextState);
-
     const lang = nextState ? 'en' : 'he';
     this.translate.use(lang);
-  }
-
-  private updateGlobalStyles(isEnglish: boolean) {
-    const dir = isEnglish ? 'ltr' : 'rtl';
-    const lang = isEnglish ? 'en' : 'he';
-
-    this.renderer.setAttribute(document.documentElement, 'dir', dir);
-    this.renderer.setAttribute(document.documentElement, 'lang', lang);
-    this.renderer.setAttribute(document.body, 'dir', dir);
-    document.body.style.direction = dir;
-
-    if (isEnglish) {
-      this.renderer.removeClass(document.body, 'rtl');
-      this.renderer.addClass(document.body, 'ltr');
-    } else {
-      this.renderer.removeClass(document.body, 'ltr');
-      this.renderer.addClass(document.body, 'rtl');
-    }
+    document.documentElement.dir = nextState ? 'ltr' : 'rtl';
+    document.documentElement.lang = lang;
   }
 }
