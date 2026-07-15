0bf54f00e553eb1365c704343bbd0bb75960c9a6
Tom <2tom.hayoun@gmail.com>
2026-07-16 01:10:23 +0300

    updating 16.07

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -63,6 +63,7 @@ export interface PaymentPreparationResponse {
     failureUrl?: string;
     email?: string;
     fullName?: string;
+    affiliation?: string;
 }
 
 @Injectable({ providedIn: 'root' })
