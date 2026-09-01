e803ced6fa7caee140ba532a4024cc7be0305a34
Tom <2tom.hayoun@gmail.com>
2026-09-01 09:00:26 +0300

    01.09.2026

--- src/app/client/components/conference-details/conference-details.component.ts
+++ src/app/client/components/conference-details/conference-details.component.ts
@@ -25,7 +25,7 @@ export class ConferenceDetailsComponent implements OnInit {
   private readonly EXCLUDED_CONFERENCE_NAMES: string[] = [
     'Law',
     'Network Dynamics in Socio-Technical Systems: From Resilient Control to Incentives and Information Design',
-    'Cancer Biology Across Scales'
+    'Cancer Biology Across Scales',
   ];
 
   constructor(
